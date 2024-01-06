d3e6bddeb4aa43f41c48ccbe05efbe4068298735
Otávio Ribeiro <otavioribeiromoreiradacosta@gmail.com>
2024-01-05 22:22:10 -0300

    chore: improved code readability

--- Matrix/matrix.go
+++ Matrix/matrix.go
@@ -8,8 +8,8 @@ import(
 
 type Matrix struct {
 
-	Rows , Cols int
-	matrix_data []float64
+	rows , cols int
+	matrixData []float64
 
 }
 
@@ -19,11 +19,11 @@ func NewMatrix() *Matrix {
 
 func NewMatrixWithSize(nRows, nCols int) *Matrix {
 
-matrixData := make([]float64, nRows*nCols)
+matrix_data := make([]float64, nRows*nCols)
 return &Matrix{
-	Rows: nRows,
-	Cols: nCols,
-	matrix_data: matrixData,
+	rows: nRows,
+	cols: nCols,
+	matrixData: matrix_data,
 }
 
 }
@@ -35,80 +35,79 @@ func NewMatrixWithData(nRows, nCols int, InputData []float64) *Matrix {
 	copy(matrixData,InputData)
 		
 	return &Matrix{
-			Rows: nRows,
-			Cols: nCols,
-			matrix_data: matrixData,
+			rows: nRows,
+			cols: nCols,
+			matrixData: matrixData,
 	
 	}
 
 }
 
 
-
 func (m *Matrix) Resize(numRows, numCols int){
-	m.Rows = numRows
-	m.Cols = numCols
-	m.matrix_data = make([]float64, numCols*numRows)
+	m.rows = numRows
+	m.cols = numCols
+	m.matrixData = make([]float64, numCols*numRows)
 }
 
 func (m *Matrix) SetToIdentity(){
 
-	if m.Cols != m.Rows{
+	if m.cols != m.rows{
 		return
 	}
 
-	for i := 0 ; i < m.Rows; i++ {
-		for j:= 0; j < m.Cols; j++{
+	for i := 0 ; i < m.rows; i++ {
+		for j:= 0; j < m.cols; j++{
 			if i == j {
-				m.matrix_data[i * m.Cols + j] = 1.0 
+				m.matrixData[i * m.cols + j] = 1.0 
 			} else {
-				m.matrix_data[i * m.Cols + j] = 0.0
+				m.matrixData[i * m.cols + j] = 0.0
 			}
 		}
 	}
 }
 
 func (m *Matrix) GetElement(nRows, nCols int) float64{
-	index := nRows * m.Cols + nCols
-	if nRows < 0 || nRows >= m.Rows || nCols < 0 || nCols >= m.Cols{
+	index := nRows * m.cols + nCols
+	if nRows < 0 || nRows >= m.rows || nCols < 0 || nCols >= m.cols{
 		return 0.0
 	}
 
-	return m.matrix_data[index] 
+	return m.matrixData[index] 
 }
 
 func (m *Matrix) GetNumRows() int {
-	return m.Rows
+	return m.rows
 }
 
 func (m *Matrix) GetNumCols() int {
-	return m.Cols
+	return m.cols
 }
 
 func (m *Matrix) SetElement(nRows, nCols int, elementValue float64 ) error {
-	index := nRows * m.Cols + nCols
-	if nRows < 0 || nRows >= m.Rows || nCols < 0 || nCols >= m.Cols{
+	index := nRows * m.cols + nCols
+	if nRows < 0 || nRows >= m.rows || nCols < 0 || nCols >= m.cols{
 		return errors.New("Index out of Bounds")
 	}
-	m.matrix_data[index] = elementValue
+	m.matrixData[index] = elementValue
 	return nil
 }
 
 func (m *Matrix) Compare(matrix1 *Matrix, tolerance float64) bool {
 
-if  matrix1.Rows != m.Rows || matrix1.Cols != m.Cols {
+if  matrix1.rows != m.rows || matrix1.cols != m.cols {
 	return false
 }
 
 var cumulativeSum float64
-numElements := m.Rows * m.Cols
+numElements := m.rows * m.cols
 for i:= 0; i < numElements ; i++ {
-	element1 := float64(matrix1.matrix_data[i])
-	element2 := float64(m.matrix_data[i])
+	element1 := float64(matrix1.matrixData[i])
+	element2 := float64(m.matrixData[i])
 	cumulativeSum += (element1 - element2) * (element1 - element2)
 }
 
-finalValue := math.Sqrt(cumulativeSum / float64(matrix1.Rows*matrix1.Cols - 1))
+finalValue := math.Sqrt(cumulativeSum / float64(matrix1.rows*matrix1.cols - 1))
 if finalValue < tolerance{
 	return true
 } 
@@ -117,29 +116,29 @@ return false
 }
 
 func (m *Matrix) IsSquare() bool {
-	if m.Rows == m.Cols {
+	if m.rows == m.cols {
 		return true
 	}
 return false
 }
 
 func (m *Matrix) FindSubMatrix(rowToRemove, colToRemove int) (*Matrix, error) {
-	if rowToRemove >= m.Rows || colToRemove >= m.Cols {
+	if rowToRemove >= m.rows || colToRemove >= m.cols {
 			return nil, errors.New("Index out of bounds")
 	}
 
-	subMatrix := NewMatrixWithSize(m.Rows-1, m.Cols-1)
+	subMatrix := NewMatrixWithSize(m.rows-1, m.cols-1)
 	subMatrixRow := 0
-	for i := 0; i < m.Rows; i++ {
+	for i := 0; i < m.rows; i++ {
 			if i == rowToRemove {
 				continue
 			}
 			subMatrixCol := 0
-			for j:= 0; j < m.Cols; j++{
+			for j:= 0; j < m.cols; j++{
 				if j == colToRemove {
 					continue
 				}
-				subMatrix.matrix_data[subMatrixRow * subMatrix.Cols + subMatrixCol] = m.GetElement(i,j)
+				subMatrix.matrixData[subMatrixRow * subMatrix.cols + subMatrixCol] = m.GetElement(i,j)
 				subMatrixCol++
 			}
 			subMatrixRow++
@@ -153,11 +152,11 @@ func (m *Matrix) Determinant() (float64, error) {
 			return 0, errors.New("Matrix is not square")
 	}
 	
-	if m.Rows == 2 {
-			return m.matrix_data[0]*m.matrix_data[3] - m.matrix_data[1]*m.matrix_data[2], nil
+	if m.rows == 2 {
+			return m.matrixData[0]*m.matrixData[3] - m.matrixData[1]*m.matrixData[2], nil
 	} else {
 			sum := 0.0
-			for i := 0; i < m.Cols; i++ {
+			for i := 0; i < m.cols; i++ {
 					subMatrix, _ := m.FindSubMatrix(0, i)
 					det, _ := subMatrix.Determinant()
 					sum += math.Pow(-1, float64(i)) * m.GetElement(0, i) * det
@@ -168,10 +167,10 @@ func (m *Matrix) Determinant() (float64, error) {
 
 
 func (m *Matrix) MatrixPrint() {
-	for i := 0; i < m.Rows; i++ {
+	for i := 0; i < m.rows; i++ {
 		fmt.Print("[")
-		for j := 0; j < m.Cols; j++ {
-			if j != m.Cols-1 {
+		for j := 0; j < m.cols; j++ {
+			if j != m.cols-1 {
 				fmt.Printf("%v , ", m.GetElement(i, j))
 			} else {
 				fmt.Printf("%v", m.GetElement(i, j)) // no comma for the last element
@@ -183,12 +182,12 @@ func (m *Matrix) MatrixPrint() {
 
 func (m *Matrix) MatrixSum(matrix1 *Matrix) (*Matrix, error) {
 
-	if m.Cols == matrix1.Cols && m.Rows == matrix1.Rows{
+	if m.cols == matrix1.cols && m.rows == matrix1.rows{
 		
-		resultMatrix := NewMatrixWithSize(m.Rows, m.Cols)
+		resultMatrix := NewMatrixWithSize(m.rows, m.cols)
 		
-		for i := 0;	i < m.Rows; i++{
-			for j:= 0; j < m.Cols; j++{
+		for i := 0;	i < m.rows; i++{
+			for j:= 0; j < m.cols; j++{
 				element := m.GetElement(i,j) + matrix1.GetElement(i,j)
 				resultMatrix.SetElement(i,j,element)
 			}
@@ -202,12 +201,12 @@ func (m *Matrix) MatrixSum(matrix1 *Matrix) (*Matrix, error) {
 
 func (m *Matrix) MatrixSubtract(matrix1 *Matrix) (*Matrix, error) {
 
-	if m.Cols == matrix1.Cols && m.Rows == matrix1.Rows{
+	if m.cols == matrix1.cols && m.rows == matrix1.rows{
 		
-		resultMatrix := NewMatrixWithSize(m.Rows, m.Cols)
+		resultMatrix := NewMatrixWithSize(m.rows, m.cols)
 		
-		for i := 0;	i < m.Rows; i++{
-			for j:= 0; j < m.Cols; j++{
+		for i := 0;	i < m.rows; i++{
+			for j:= 0; j < m.cols; j++{
 				element := m.GetElement(i,j) - matrix1.GetElement(i,j)
 				resultMatrix.SetElement(i,j,element)
 			}
@@ -222,12 +221,12 @@ func (m *Matrix) MatrixSubtract(matrix1 *Matrix) (*Matrix, error) {
 
 func (m *Matrix) HadamardProduct(matrix1 *Matrix) (*Matrix,error) {
 
-if m.Cols == matrix1.Cols && m.Rows == matrix1.Rows {
+if m.cols == matrix1.cols && m.rows == matrix1.rows {
 
-	resultMatrix := NewMatrixWithSize(m.Cols, m.Rows)
+	resultMatrix := NewMatrixWithSize(m.cols, m.rows)
 
-	for i := 0; i < m.Cols; i++{
-		for j:= 0; j < m.Rows; j++{
+	for i := 0; i < m.cols; i++{
+		for j:= 0; j < m.rows; j++{
 			product := m.GetElement(i,j) * matrix1.GetElement(i,j)
 			resultMatrix.SetElement(i,j,product)
 		}
@@ -241,16 +240,16 @@ return nil, errors.New("Matrix dimensions do not match for Hadamard product")
 }
 
 func (m *Matrix) MatrixMultiply(matrix1 *Matrix) (*Matrix, error) {
-	if m.Cols != matrix1.Rows {
+	if m.cols != matrix1.rows {
 			return nil, errors.New("Matrix dimensions are not compatible for matrix multiplication")
 	}
 
-	resultMatrix := NewMatrixWithSize(m.Rows, matrix1.Cols)
+	resultMatrix := NewMatrixWithSize(m.rows, matrix1.cols)
 
-	for i := 0; i < m.Rows; i++ {
-			for j := 0; j < matrix1.Cols; j++ {
+	for i := 0; i < m.rows; i++ {
+			for j := 0; j < matrix1.cols; j++ {
 					var sum float64
-					for k := 0; k < m.Cols; k++ {
+					for k := 0; k < m.cols; k++ {
 							sum += m.GetElement(i, k) * matrix1.GetElement(k, j)
 					}
 					resultMatrix.SetElement(i, j, sum)
